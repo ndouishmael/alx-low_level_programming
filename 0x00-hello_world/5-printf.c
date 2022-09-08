fe9cf2ed5085c4912f7207b942edf5937a18bb82
ndouishmael <ndouishmael@gmail.com>
2022-09-08 07:22:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,9 +9,7 @@
  *  followed by a newline, using printf and not puts.
  *
  *  Return: Always 0 (Success)
- *
- *      */
-
+ */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
