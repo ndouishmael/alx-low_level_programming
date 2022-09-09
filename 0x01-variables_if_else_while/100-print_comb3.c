c451201e9679e52b6118bd27b0df157780c06406
ndouishmael <ndouishmael@gmail.com>
2022-09-09 05:59:36 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,23 +11,22 @@ int main(void)
 
 	for (i = '0'; i <= '8'; i++)
 	{
-	for (j = i + 1; j <= '9'; j++)								
-	{
-	if (i != j)												  
-	{
-		putchar(i);
-		putchar(j);
-		if (i != '8' || j != '9')
+		for (j = i + 1; j <= '9'; j++)
 		{
-			putchar(',');
-			putchar(' ');
-		} 
-	} 
-	}  
-	} 
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != '8' || j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
 	putchar('\n');
 
 	return (0);
 }
-
