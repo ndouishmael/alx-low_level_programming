966b65a1b8a982d0ab7ec41487f7ea19c483ef35
ndouishmael <ndouishmael@gmail.com>
2022-09-13 10:52:17 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,12 @@ void print_alphabet_x10(void)
 {
 	int ch, i;
 
-        for (i = 0; i < 10; i++)
-	{ 
-        for (ch = 'a'; ch <= 'z'; ch++)
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
+
 		}
 		_putchar('\n');
 	}
