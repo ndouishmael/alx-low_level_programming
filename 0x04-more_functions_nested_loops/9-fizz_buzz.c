21e7ee842a7497ee71d8a0f796b29e09482826d9
ndouishmael <ndouishmael@gmail.com>
2022-09-15 12:07:31 -0700

    fiz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,18 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
-		if (i % 3 != 0 && i % 5 != 0)
-		{
-			printf("%d ", i);
-		}
-		else 
-		{
-			if (i % 3 == 0)
-			{
-				printf("Fizz");
-			}
-			if (i % 5 == 0)
-			{
-				printf("Buzz");
-			}
-			(i == 100) ? putchar('\n') : putchar(' ');
-		}
-
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i != 100)
+			printf(" ");
 	}
+
+	printf("\n");
 	return (0);
 }
