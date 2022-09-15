d88ad34c7dcace64b69af488d21e5132d4526069
ndouishmael <ndouishmael@gmail.com>
2022-09-15 12:59:53 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,40 +10,16 @@
 
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
+	unsigned int num = n;
 
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	if (n < 0)
 	{
-		_putchar (n + '0');
+		putchar('-');
+		num = -num;
 	}
-	else if (n > -10 && n < 0)
+	if (num > 9)
 	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
+		print_number(num / 10);
 	}
-
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-		while (z / b > 9)
-		{
-			b = b * 10;
-		}
-		while (b > 0)
-		{
-			m = z / b;
-			z = z % b;
-			_putchar (m + '0');
-			b = b / 10;
-		}
-	}
+	putchar(num % 10 + '0');
 }
