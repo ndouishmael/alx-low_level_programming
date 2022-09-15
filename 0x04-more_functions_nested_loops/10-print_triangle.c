fdb4f4d40f82ac70def57b077cf688df8c79a025
ndouishmael <ndouishmael@gmail.com>
2022-09-15 11:59:36 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *  print_triangle - Entry point
+ *  Desc: Entry
+ *  @size: int var
+ *  Return: Always 0 (Success)
+ */
+
+void print_triangle(int size)
+{
+	int c;
+	int l;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (l = 0 ; l < size ; l++)
+	{
+		for (c = 0 ; c < size ; c++)
+		{
+			if (c >= size - l - 1)
+			{
+				_putchar('#');
+			}
+			else 
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
