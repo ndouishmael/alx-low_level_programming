04e8cb91927119043cebe81947d1667461bb4897
ndouishmael <ndouishmael@gmail.com>
2022-09-15 11:17:38 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * Desc: Entry
+ * @a: int var
+ * @b: int var
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
