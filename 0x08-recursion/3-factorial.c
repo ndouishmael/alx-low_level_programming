13167e7e5a916120b8e2ef8ca8426a4b30046a2c
ndouishmael <ndouishmael@gmail.com>
2022-09-28 09:01:50 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - a function that finds the factorial of *n
+ *
+ * @n: positive number
+ *
+ * Return: factorial of @n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
