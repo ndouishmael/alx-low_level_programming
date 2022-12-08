0729fd6b99aef7e98157a62ca5f6d6f66365c224
ndouishmael <111059267+ndouishmael@users.noreply.github.com>
2022-12-08 18:07:37 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,40 +1,48 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts node at given index
- * @h: address of pointer to current head node
- * @idx: the index to at which to insert
- * @n: the value of the inserted node
- *
- * Return: new node or NULL
+ * insert_dnodeint_at_index - This function inserts a new node
+ * at a given position
+ * @h: The head node of the linked list
+ * @idx: The index where the new node should be
+ * @n: The int element value of a node
+ * Return: The address of the new node, or NULL if it failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t)), *node;
+	dlistint_t *new_node = NULL, *tmp = NULL;
 
-	if (!h || !new)
-		return (new ? free(new), NULL : NULL);
-	node = *h;
-	new->n = n;
+	/* check if value of idx is NULL, add node at the beginning */
 	if (!idx)
+		return (add_dnodeint(h, n));
+
+	/* check if pointer is NULL */
+	if (!h)
+		return (NULL);
+
+	tmp = *h;
+	while (tmp && idx > 1 && tmp->next)
 	{
-		new->prev = NULL;
-		new->next = node ? node : NULL;
-		if (node)
-			node->prev = new;
-		return (*h = new);
+		tmp = tmp->next;
+		idx--;
 	}
-	for (; node; node = node->next, idx--)
+
+	if (!tmp || idx > 1)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node)
 	{
-		if (idx - 1 == 0)
-		{
-			new->prev = node;
-			new->next = node->next;
-			if (new->next)
-				new->next->prev = new;
-			node->next = new;
-			return (new);
-		}
+		new_node->n = n;
+		new_node->prev = tmp;
+		new_node->next = tmp->next;
+		if (tmp->next) /* connect next node's previous to new_node */
+			tmp->next->prev = new_node;
+		tmp->next = new_node;
 	}
-	return (free(new), NULL);
+	else
+		return (NULL);
+
+	return (new_node);
 }
